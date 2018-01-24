0f87bae4cdc5c73f596a455d19b17680be84d97a
RockyChing <zdh1021680772@163.com>
2018-01-24 11:25:30 +0800

    add socket common test

---
+++ src/socket/sockets_test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+
+#include <sockets.h>
+#include <log_util.h>
+
+
+int socket_common_test()
+{
+	func_enter();
+
+
+
+	func_exit();
+	return 0;
+}
+
