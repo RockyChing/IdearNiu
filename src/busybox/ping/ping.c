adb523d60288bc28c7bd68a64d147b3a22e9bdef
RockyChing <zdh1021680772@163.com>
2019-07-03 11:15:05 +0800

    Update ping.c

--- src/busybox/ping/ping.c
+++ src/busybox/ping/ping.c
@@ -1,36 +1,21 @@
-/*
- * Port from Busybox version: 1.22.1
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <signal.h>
-#include <errno.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
+#include <common_header.h>
 #include <net/if.h>
+#include <netinet/in.h>
 #include <netinet/ip_icmp.h>
-#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <time.h>
+#define LOG_TAG "PING"
 
-#include "log_util.h"
-#include "xfuncs.h"
-#include "libbb.h"
+#include <log_util.h>
 
-#ifndef UINT_MAX
-#define UINT_MAX	(~0U)
-#endif
+typedef struct len_and_sockaddr {
+	union {
+		struct sockaddr sa;
+		struct sockaddr_in sin;
+	} u;
+} len_and_sockaddr;
 
-
-/**
- * 0 - simple version
- * 1 - full version
- */
-#define ENABLE_FEATURE_FANCY_PING 1
 
 enum {
 	DEFDATALEN = 56,
@@ -38,9 +23,26 @@ enum {
 	MAXICMPLEN = 76,
 	MAX_DUP_CHK = (8 * 128),
 	MAXWAIT = 10,
-	PINGINTERVAL = 1, /* 1 second */
-	pingsock = 0,
+	PINGINTERVAL = 10, /* 10 second */
 };
+
+static len_and_sockaddr g_las;
+
+static void* xzalloc(size_t size)
+{
+	void *ptr = malloc(size);
+	if (ptr == NULL && size != 0)
+		log_fatal("bb_msg_memory_exhausted");
+	memset(ptr, 0, size);
+	return ptr;
+}
+
+static unsigned long long monotonic_us(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000ULL + tv.tv_usec;
+}
 
 /**
  * return true if little endian
@@ -85,209 +87,90 @@ static uint16_t inet_cksum(uint16_t *addr, int nleft)
 	return (uint16_t)~sum;
 }
 
-/* host: "1.2.3.4[:port]", "www.google.com[:port]"
- * port: if neither of above specifies port # */
-static len_and_sockaddr* str2sockaddr(const char *host, int port, sa_family_t af)
+static int setsockopt_int(int fd, int level, int optname, int optval)
 {
-	af = AF_INET;
-	int rc;
-	len_and_sockaddr *r = NULL;
-	struct addrinfo *result = NULL;
-	struct addrinfo *used_res;
-	const char *org_host = host; /* only for error msg */
-	struct addrinfo hint;
-
-	memset(&hint, 0 , sizeof(hint));
-	hint.ai_family = af;
-	/* Need SOCK_STREAM, or else we get each address thrice (or more)
-	 * for each possible socket type (tcp,udp,raw...): */
-	hint.ai_socktype = SOCK_STREAM;
-	rc = getaddrinfo(host, NULL, &hint, &result);
-	if (rc || !result) {
-		error("bad address '%s'", org_host);
-		goto ret;
-	}
-
-	used_res = result;
-	r = malloc(LSA_LEN_SIZE + used_res->ai_addrlen);
-	r->len = used_res->ai_addrlen;
-	memcpy(&r->u.sa, used_res->ai_addr, used_res->ai_addrlen);
-
- ret:
-	if (result)
-		freeaddrinfo(result);
-	return r;
+	return setsockopt(fd, level, optname, &optval, sizeof(int));
 }
 
-static len_and_sockaddr *xhost_and_af2sockaddr(const char *host, int port, sa_family_t af)
+static int setsockopt_1(int fd, int level, int optname)
 {
-	return str2sockaddr(host, port, af);
+	return setsockopt_int(fd, level, optname, 1);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-static void create_icmp_socket(void)
-#define create_icmp_socket(lsa) create_icmp_socket()
+static int setsockopt_SOL_SOCKET_int(int fd, int optname, int optval)
 {
-	int sock = socket(AF_INET, SOCK_RAW, 1); /* 1 == ICMP */
-	if (sock < 0) {
-		if (errno != EPERM)
-			error("bb_msg_can_not_create_raw_socket");
-		/* We don't have root privileges.  Try SOCK_DGRAM instead.
-		 * Linux needs net.ipv4.ping_group_range for this to work.
-		 * MacOSX allows ICMP_ECHO, ICMP_TSTAMP or ICMP_MASKREQ
-		 */
-		sock = socket(AF_INET, SOCK_DGRAM, 1); /* 1 == ICMP */
-		if (sock < 0)
-			error("bb_msg_perm_denied_are_you_root");
+	return setsockopt_int(fd, SOL_SOCKET, optname, optval);
+}
+
+static int sock_close(int *sockfd)
+{
+	close(*sockfd);
+	*sockfd = -1;
+	return 0;
+}
+
+static int sock_set_blocking(int sockfd, int block)
+{
+	int res;
+	int flags;
+	flags = fcntl(sockfd, F_GETFL, 0);
+	if (flags < 0) {
+		error("F_GETFL on socket %d failed", sockfd);
+		return -1;
 	}
 
-	if (sock < 0) {
-		error("cannot create_icmp_socket");
-		abort();
+	if (block) {
+		flags &= ~O_NONBLOCK;
 	} else {
-		xmove_fd(sock, pingsock);
+		flags |= O_NONBLOCK;
 	}
+
+	res = fcntl(sockfd, F_SETFL, flags);
+	if (res < 0) {
+		error("F_SETFL on socket %d failed", sockfd);
+		return -1;
+	}
+
+	return res;
 }
 
-#if !ENABLE_FEATURE_FANCY_PING
-
-/* Simple version */
-
-struct globals {
-	char *hostname;
-	char packet[DEFDATALEN + MAXIPLEN + MAXICMPLEN];
-};
-
-static struct globals global_data = { 0 };
-static uint16_t nsend = 0;
-#define G (*(struct globals*)&global_data)
-#define INIT_G() do { } while (0)
-
-static void noresp(int ign)
+static ssize_t xsendto(int s, const void *buff, size_t len, const struct sockaddr *to,
+				socklen_t tolen)
 {
-	printf("No response from %s\n", G.hostname);
-	exit(0);
-}
+	ssize_t t, retry;
 
-static void ping4(len_and_sockaddr *lsa)
-{
-	struct icmp *pkt;
-	int c;
+	retry = 0;
+	for (t = 0 ; len > 0 ; ) {
+		int n = sendto(s, buff + t, len, 0, to, tolen);
+		if (n < 0) {
+			int err_bak = errno;
+			if ((err_bak == EAGAIN || err_bak == EINTR || err_bak == EINPROGRESS) && retry < 20) {
+				retry ++;
+				usleep(50000);
+				continue;
+			}
 
-	pkt = (struct icmp *) G.packet;
-	memset(pkt, 0, sizeof(G.packet));
-	pkt->icmp_type = ICMP_ECHO;
-#if 1
-	/* fill data */
-	pkt->icmp_code = 0;
-	info("getpid: %d", getpid());
-	pkt->icmp_id = getpid();
-	pkt->icmp_seq = (uint16_t)(nsend ++);
-	memset(pkt->icmp_data, 0xa5, 56); /* fill with pattern */
-#endif
-	pkt->icmp_cksum = inet_cksum((uint16_t *) pkt, sizeof(G.packet));
-
-	info("send:");
-	dump(G.packet, (DEFDATALEN + ICMP_MINLEN));
-	xsendto(pingsock, G.packet, DEFDATALEN + ICMP_MINLEN, &lsa->u.sa, lsa->len);
-
-	/* listen for replies */
-	while (1) {
-		c = recv(pingsock, G.packet, sizeof(G.packet), 0);
-		if (c < 0) {
-			if (errno != EINTR)
-				error("recvfrom");
-			continue;
+			log_debug("socket send error: %s", strerror(err_bak));
+		    return -1;
 		}
-
-		info("\nrecv:");
-		dump(G.packet, c);
-		if (c >= 76) {			/* ip + icmp */
-			struct iphdr *iphdr = (struct iphdr *) G.packet;
-
-			pkt = (struct icmp *) (G.packet + (iphdr->ihl << 2));	/* skip ip hdr */
-			if (pkt->icmp_type == ICMP_ECHOREPLY)
-				break;
-		}
-	}
-	if (ENABLE_FEATURE_CLEAN_UP)
-		close(pingsock);
-}
-
-#define common_ping_main(af, argv) common_ping_main(argv)
-
-static int common_ping_main(sa_family_t af, char **argv)
-{
-	len_and_sockaddr *lsa;
-
-	INIT_G();
-	argv++;
-
-	G.hostname = *argv;
-	if (!G.hostname) {
-		error("ping need HOSTNAME");
-		abort();
+		t += n;
+		len -= n;
+		retry = 0;
 	}
 
-	lsa = xhost_and_af2sockaddr(G.hostname, 0, AF_INET);
-	/* Set timer _after_ DNS resolution */
-	signal(SIGALRM, noresp);
-	alarm(5); /* give the host 5000ms to respond */
-
-	create_icmp_socket(lsa);
-	ping4(lsa);
-	printf("%s is alive!\n", G.hostname);
-	return EXIT_SUCCESS;
+	return t;
 }
 
-#else /* FEATURE_FANCY_PING */
-#include "time_util.h"
-
-/* Full(er) version */
-#define OPT_STRING ("qvc:s:t:w:W:I:n4" IF_PING6("6"))
-enum {
-	OPT_QUIET = 1 << 0,
-	OPT_VERBOSE = 1 << 1,
-	OPT_c = 1 << 2,
-	OPT_s = 1 << 3,
-	OPT_t = 1 << 4,
-	OPT_w = 1 << 5,
-	OPT_W = 1 << 6,
-	OPT_I = 1 << 7,
-	/*OPT_n = 1 << 8, - ignored */
-	OPT_IPV4 = 1 << 9,
-};
-
-
-struct globals {
-	int if_index;
-	char *str_I;
-	len_and_sockaddr *source_lsa;
+struct ping_struct {
+	int pingsock;
 	unsigned datalen;
-	unsigned pingcount; /* must be int-sized */
 	unsigned opt_ttl;
 	unsigned long ntransmitted, nreceived, nrepeats;
 	uint16_t myid;
+	uint8_t pattern;
 	unsigned tmin, tmax; /* in us */
 	unsigned long long tsum; /* in us, sum of all times */
-	unsigned deadline;
 	unsigned timeout;
-	unsigned total_secs;
 	unsigned sizeof_rcv_packet;
 	char *rcv_packet; /* [datalen + MAXIPLEN + MAXICMPLEN] */
 	void *snd_packet; /* [datalen + ipv4/ipv6_const] */
@@ -298,34 +181,21 @@ struct globals {
 		struct sockaddr_in sin;
 	} pingaddr;
 	unsigned char rcvd_tbl[MAX_DUP_CHK / 8];
-};
+} FIX_ALIASING;
+struct ping_struct G;
 
-static struct globals global_data = { 0 };
-#define G (*(struct globals*)&global_data)
-#define if_index     (G.if_index    )
-#define source_lsa   (G.source_lsa  )
-#define str_I        (G.str_I       )
+#define pingsock     (G.pingsock     )
 #define datalen      (G.datalen     )
-#define pingcount    (G.pingcount   )
 #define opt_ttl      (G.opt_ttl     )
 #define myid         (G.myid        )
 #define tmin         (G.tmin        )
 #define tmax         (G.tmax        )
 #define tsum         (G.tsum        )
-#define deadline     (G.deadline    )
 #define timeout      (G.timeout     )
-#define total_secs   (G.total_secs  )
 #define hostname     (G.hostname    )
 #define dotted       (G.dotted      )
 #define pingaddr     (G.pingaddr    )
 #define rcvd_tbl     (G.rcvd_tbl    )
-
-#define INIT_G() do { \
-	datalen = DEFDATALEN; \
-	timeout = MAXWAIT; \
-	tmin = UINT_MAX; \
-} while (0)
-
 
 #define BYTE(bit)	rcvd_tbl[(bit)>>3]
 #define MASK(bit)	(1 << ((bit) & 7))
@@ -360,56 +230,37 @@ static void print_stats_and_exit(int junk)
 			tmax / 1000, tmax % 1000);
 	}
 	/* if condition is true, exit with 1 -- 'failure' */
-	exit(nrecv == 0 || (deadline && nrecv < pingcount));
+	exit(nrecv == 0);
 }
 
-static void sendping_tail(void (*sp)(int), int size_pkt)
+static void ping_socket_create(void)
 {
-	int sz;
+	int sock;
 
-	CLR((uint16_t)G.ntransmitted % MAX_DUP_CHK);
-	G.ntransmitted++;
+	do {
+		if (pingsock > 0)
+			break;
 
-	size_pkt += datalen;
-
-	/* sizeof(pingaddr) can be larger than real sa size, but I think
-	 * it doesn't matter */
-	sz = xsendto(pingsock, G.snd_packet, size_pkt, &pingaddr.sa, sizeof(pingaddr));
-	if (sz != size_pkt)
-		error("bb_msg_write_error");
-
-	if (pingcount == 0 || deadline || G.ntransmitted < pingcount) {
-		/* Didn't send all pings yet - schedule next in 1s */
-		signal(SIGALRM, sp);
-		if (deadline) {
-			total_secs += PINGINTERVAL;
-			if (total_secs >= deadline)
-				signal(SIGALRM, print_stats_and_exit);
+		sock = socket(AF_INET, SOCK_RAW, 1); /* 1 == ICMP */
+		if (sock < 0) {
+			if (errno == EPERM)
+				log_fatal("bb_msg_perm_denied_are_you_root");
+			log_fatal("bb_msg_can_not_create_raw_socket");
+			sleep(1);
+			continue;
 		}
-		alarm(PINGINTERVAL);
-	} else { /* -c NN, and all NN are sent (and no deadline) */
-		/* Wait for the last ping to come back.
-		 * -W timeout: wait for a response in seconds.
-		 * Affects only timeout in absense of any responses,
-		 * otherwise ping waits for two RTTs. */
-		unsigned expire = timeout;
 
-		if (G.nreceived) {
-			/* approx. 2*tmax, in seconds (2 RTT) */
-			expire = tmax / (512*1024);
-			if (expire == 0)
-				expire = 1;
-		}
-		signal(SIGALRM, print_stats_and_exit);
-		alarm(expire);
-	}
+		log_debug("create_icmp_socket OK");
+		pingsock = sock;
+		sock_set_blocking(pingsock, 0);
+	} while (1);
 }
 
 static void sendping4(int junk)
 {
 	struct icmp *pkt = G.snd_packet;
 
-	//memset(pkt, 0, datalen + ICMP_MINLEN + 4); - G.snd_packet was xzalloced
+	memset(pkt, G.pattern, datalen + ICMP_MINLEN + 4);
 	pkt->icmp_type = ICMP_ECHO;
 	/*pkt->icmp_code = 0;*/
 	pkt->icmp_cksum = 0; /* cksum is calculated with this field set to 0 */
@@ -425,27 +276,17 @@ static void sendping4(int junk)
 
 	pkt->icmp_cksum = inet_cksum((uint16_t *) pkt, datalen + ICMP_MINLEN);
 
-	sendping_tail(sendping4, ICMP_MINLEN);
-}
+	int size_pkt = datalen + ICMP_MINLEN;
+	int sz;
 
-static const char *icmp_type_name(int id)
-{
-	switch (id) {
-	case ICMP_ECHOREPLY:      return "Echo Reply";
-	case ICMP_DEST_UNREACH:   return "Destination Unreachable";
-	case ICMP_SOURCE_QUENCH:  return "Source Quench";
-	case ICMP_REDIRECT:       return "Redirect (change route)";
-	case ICMP_ECHO:           return "Echo Request";
-	case ICMP_TIME_EXCEEDED:  return "Time Exceeded";
-	case ICMP_PARAMETERPROB:  return "Parameter Problem";
-	case ICMP_TIMESTAMP:      return "Timestamp Request";
-	case ICMP_TIMESTAMPREPLY: return "Timestamp Reply";
-	case ICMP_INFO_REQUEST:   return "Information Request";
-	case ICMP_INFO_REPLY:     return "Information Reply";
-	case ICMP_ADDRESS:        return "Address Mask Request";
-	case ICMP_ADDRESSREPLY:   return "Address Mask Reply";
-	default:                  return "unknown ICMP type";
-	}
+	CLR((uint16_t)G.ntransmitted % MAX_DUP_CHK);
+	G.ntransmitted++;
+
+	/* sizeof(pingaddr) can be larger than real sa size, but I think
+	 * it doesn't matter */
+	sz = xsendto(pingsock, G.snd_packet, size_pkt, &pingaddr.sa, sizeof(pingaddr));
+	if (sz != size_pkt)
+		log_fatal("bb_msg_write_error");
 }
 
 static void unpack_tail(int sz, uint32_t *tp,
@@ -510,13 +351,10 @@ static void unpack4(char *buf, int sz, struct sockaddr_in *from)
 
 		if (sz >= ICMP_MINLEN + sizeof(uint32_t))
 			tp = (uint32_t *) icmppkt->icmp_data;
-		unpack_tail(sz, tp,
-			inet_ntoa(*(struct in_addr *) &from->sin_addr.s_addr),
+		unpack_tail(sz, tp, dotted,
 			recv_seq, iphdr->ttl);
 	} else if (icmppkt->icmp_type != ICMP_ECHO) {
-		error("warning: got ICMP %d (%s)",
-				icmppkt->icmp_type,
-				icmp_type_name(icmppkt->icmp_type));
+		log_fatal("warning: got ICMP %d", icmppkt->icmp_type);
 	}
 }
 
@@ -525,95 +363,111 @@ static void ping4(len_and_sockaddr *lsa)
 	int sockopt;
 
 	pingaddr.sin = lsa->u.sin;
-	if (source_lsa) {
-		if (setsockopt(pingsock, IPPROTO_IP, IP_MULTICAST_IF,
-				&source_lsa->u.sa, source_lsa->len))
-			error("can't set multicast source interface");
-		xbind(pingsock, &source_lsa->u.sa, source_lsa->len);
-	}
+
+	/* enable broadcast pings */
+	// setsockopt_broadcast(pingsock);
 
 	/* set recv buf (needed if we can get lots of responses: flood ping,
 	 * broadcast ping etc) */
 	sockopt = (datalen * 2) + 7 * 1024; /* giving it a bit of extra room */
-	setsockopt(pingsock, SOL_SOCKET, SO_RCVBUF, &sockopt, sizeof(sockopt));
+	setsockopt_SOL_SOCKET_int(pingsock, SO_RCVBUF, sockopt);
 
 	if (opt_ttl != 0) {
-		setsockopt(pingsock, IPPROTO_IP, IP_TTL, &opt_ttl, sizeof(opt_ttl));
-		/* above doesnt affect packets sent to bcast IP, so... */
-		setsockopt(pingsock, IPPROTO_IP, IP_MULTICAST_TTL, &opt_ttl, sizeof(opt_ttl));
+		setsockopt_int(pingsock, IPPROTO_IP, IP_TTL, opt_ttl);
+		/* above doesn't affect packets sent to bcast IP, so... */
+		setsockopt_int(pingsock, IPPROTO_IP, IP_MULTICAST_TTL, opt_ttl);
 	}
-
-	signal(SIGINT, print_stats_and_exit);
 
 	/* start the ping's going ... */
 	sendping4(0);
 
 	/* listen for replies */
-	while (1) {
+	while (pingsock > 0) {
 		struct sockaddr_in from;
 		socklen_t fromlen = (socklen_t) sizeof(from);
-		int c;
 
-		c = recvfrom(pingsock, G.rcv_packet, G.sizeof_rcv_packet, 0,
-				(struct sockaddr *) &from, &fromlen);
-		if (c < 0) {
-			if (errno != EINTR)
-				error("recvfrom");
-			continue;
-		}
-		unpack4(G.rcv_packet, c, &from);
-		if (pingcount && G.nreceived >= pingcount)
+		struct timeval tv;
+		fd_set rfds;
+		int ret, c;
+		int err_bak;
+
+		memset(&tv, 0, sizeof(tv));
+		FD_ZERO(&rfds);
+		FD_SET(pingsock, &rfds);
+
+		tv.tv_sec = timeout;
+		tv.tv_usec = 0;
+		ret = select(pingsock + 1, &rfds, NULL, NULL, &tv);
+		if (ret > 0) {
+			if (FD_ISSET(pingsock, &rfds)) {
+				c = recvfrom(pingsock, G.rcv_packet, G.sizeof_rcv_packet, 0, (struct sockaddr *) &from, &fromlen);
+				if (c < 0) {
+					err_bak = errno;
+					if (err_bak == EAGAIN || err_bak == EINTR || err_bak == EINPROGRESS) {
+						usleep(500000);
+						continue;
+					}
+
+					log_fatal("recvfrom failed: %s", strerror(errno));
+					sock_close(&pingsock);
+					break;
+				}
+
+				log_debug("recv %d bytes", c);
+				unpack4(G.rcv_packet, c, &from);
+				if (c == 84)
+					break;
+			}
+		} else if (ret == 0) {
+			/* time expires, send data */
+			debug("no data in 5 seconds");
+			sock_close(&pingsock);
 			break;
+		} else {
+			log_fatal("select() complains: %s", strerror(errno));
+			sock_close(&pingsock);
+			break;
+		}
 	}
 }
 
-static void ping(len_and_sockaddr *lsa)
+static void ping_addr(const char *ip_dotted)
 {
-	printf("PING %s (%s)", hostname, dotted);
-	if (source_lsa) {
-		//printf(" from %s",
-		//	xmalloc_sockaddr2dotted_noport(&source_lsa->u.sa));
-	}
-	printf(": %d data bytes\n", datalen);
+	struct sockaddr_in *psin = &g_las.u.sin;
+	psin->sin_family = AF_INET; /* address family */
+	psin->sin_port = ntohs(0);  /* port number */
+	psin->sin_addr.s_addr = inet_addr(ip_dotted);
+}
 
-	create_icmp_socket(lsa);
+static void ping_init(void)
+{
+	pingsock = -1;
+	datalen = DEFDATALEN;
+	timeout = 5;
+	tmin = UINT_MAX;
+
+	myid = (uint16_t) getpid();
+	hostname = "test-api-cnca.midea.com";
+	dotted = "47.96.39.195";
 
 	G.sizeof_rcv_packet = datalen + MAXIPLEN + MAXICMPLEN;
 	G.rcv_packet = xzalloc(G.sizeof_rcv_packet);
 	G.snd_packet = xzalloc(datalen + ICMP_MINLEN + 4);
-	ping4(lsa);
+
+	ping_addr(dotted);
 }
 
-static int common_ping_main(int opt, char **argv)
+int ping_main(int argc, char **argv)
 {
-	len_and_sockaddr *lsa;
+	signal(SIGINT, print_stats_and_exit);
+	ping_init();
 
-	INIT_G();
+	while (1) {
+		ping_socket_create();
+		ping4(&g_las);
+		sleep(5);
+	}
 
-	//opt |= getopt32(argv, OPT_STRING, &pingcount, &str_s, &opt_ttl, &deadline, &timeout, &str_I);
-	pingcount = 3;
-	myid = (uint16_t) getpid();
-	hostname = argv[optind];
-	lsa = xhost_and_af2sockaddr(hostname, 0, AF_INET);
-
-	if (source_lsa && source_lsa->u.sa.sa_family != lsa->u.sa.sa_family)
-		/* leaking it here... */
-		source_lsa = NULL;
-
-	dotted = xmalloc_sockaddr2dotted_noport(&lsa->u.sa);
-	ping(lsa);
-	print_stats_and_exit(0);
 	return 0;
-}
-#endif /* FEATURE_FANCY_PING */
-
-
-int main(int argc, char **argv)
-{
-#if !ENABLE_FEATURE_FANCY_PING
-	return common_ping_main(AF_UNSPEC, argv);
-#else
-	return common_ping_main(0, argv);
-#endif
 }
 
